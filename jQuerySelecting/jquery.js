a5ef36a6d7b62df44826a4d99ab4bc77380dae82
BIT student <classroom@bgit.rs>
2018-11-07 13:11:28 +0100

    jquery

--- jQuerySelecting/jquery.js
+++ jQuerySelecting/jquery.js
@@ -2,3 +2,13 @@ $(document).ready(function () {
     console.log('hi');
 });
 
+var firstList = $('li:first').css('border-bottom', '4px solid black');
+
+var allLists = $('li').css('text-transform', 'uppercase');
+
+var activeEle = $('li.active').css('color', 'red');
+
+
+var ln = $('ul li').length;
+ln = (ln - 1) / 2;
+$("ul li:eq(" + ln + ")").css('background-color', 'blue');
